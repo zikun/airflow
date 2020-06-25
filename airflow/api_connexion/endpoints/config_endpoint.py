77edc52a7c1fb327691075fef2c55c08d9913569
zikun <33176974+zikun@users.noreply.github.com>
2020-06-25 14:36:19 +0800

    Remove text/plain response type

--- airflow/api_connexion/endpoints/config_endpoint.py
+++ airflow/api_connexion/endpoints/config_endpoint.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from flask import Response, request
+from flask import Response
 
 from airflow.api_connexion.schemas.config_schema import Config, ConfigOption, ConfigSection, config_schema
 from airflow.configuration import conf
@@ -25,8 +25,6 @@ def get_config() -> Response:
     """
     Get current configuration.
     """
-    response_types = ['text/plain', 'application/json']
-    content_type = request.accept_mimetypes.best_match(response_types)
     conf_dict = conf.as_dict(display_source=True, display_sensitive=True)
     config = Config(
         sections=[
@@ -40,13 +38,4 @@ def get_config() -> Response:
             for section, parameters in conf_dict.items()
         ]
     )
-    if content_type == 'text/plain':
-        config_text = '\n'.join(
-            f'[{config_section.name}]\n' +
-            ''.join(f'{config_option.key} = {config_option.value}  # source: {config_option.source}\n'
-                    for config_option in config_section.options)
-            for config_section in config.sections
-        )
-        return config_text
-    else:
-        return config_schema.dump(config)
+    return config_schema.dump(config)
